151b2c1ed9d147b6551bbbba742050486ddaf958
Carla S. Nonaka <carlasnonaka@gmail.com>
2022-09-16 00:29:18 -0300

    'commit inicial'

---
+++ src/main/java/com/example/projetoCestas/ProjetoCestasApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoCestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCestasApplication.class, args);
+	}
+
+}
